aba9137f977e9340eb7c201766b0b89c123f81fe
trialpages <163979003+trialpages@users.noreply.github.com>
2026-01-09 22:39:11 +0300

    Create lib/main.dart

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(title: Text('Benim İlk Uygulamam')),
+      body: Center(child: Text('Merhaba Dünya!')),
+    ),
+  ));
+}
